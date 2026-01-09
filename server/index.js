e0b2ca4cb480709ce60fcf0dcfb9fc5e94cc1390
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2026-01-08 23:57:48 -0800

    cors installed postman connect

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
-const express= require("express");
-const app= express();
-require ("dotenv").config();
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
-app.get('/',function(req, res){
-    res.send("hey every one welcome back to new project")
+const app = express();
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("hellow everyone welcome to first routes ");
 });
 
+app.post("/test", (req, res) => {
+  res.json({
+    message: "Data received",
+    data: req.body,
+  });
+});
+
+// SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
