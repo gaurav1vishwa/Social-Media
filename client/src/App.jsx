f8a5814a08f299c0b581801a9b5435ee69648983
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2026-01-10 03:28:34 -0800

    frontened setup successfully and register page done

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import Register from "./pages/resister";
+function App() {
+  return (
+<div>
+  <Register/>
+</div>
+)}
+
+export default App;
---
+++ client/src/pages/resister.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import API from "../services/api";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await API.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Registration successful! Please login.");
+    } catch (err) {
+      setError(err.response?.data?.message || "Something went wrong");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-800 p-8 rounded-lg w-full max-w-md"
+      >
+        <h2 className="text-2xl font-bold text-white mb-6 text-center">
+          Register
+        </h2>
+
+        {error && <p className="text-red-400 mb-3">{error}</p>}
+
+        <input
+          type="text"
+          placeholder="Name"
+          className="w-full p-3 mb-3 rounded bg-gray-700 text-white"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-3 mb-3 rounded bg-gray-700 text-white"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-3 mb-5 rounded bg-gray-700 text-white"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="w-full bg-green-600 hover:bg-green-700 text-white p-3 rounded">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
